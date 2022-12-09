93b34c0fbd456c377fb8b76653891f9529161f74
Simeon Marchand <simeonrmarchand@gmail.com>
2022-12-09 02:53:26 -0600

    basic intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 function Intro(){
   return(
-    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">Intro</div>
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Simeon Marchand</h1>
+      <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
+      <p className="text-small mac-w-cl mb-6 font-bold">I am a currently a 
+      student looking to graduate with my Associates of Software Application 
+      Development in June 2023. I will then continue my education in a 
+      Bachelors of Computer Science Program with an expected graduation date of 
+      December 2024. 
+      </p>
+    </div>
   )
 }
 
