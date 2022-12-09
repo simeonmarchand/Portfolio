514d24e989d1ba4d1b3d1d9ed8aabea9072dcddd
Simeon Marchand <simeonrmarchand@gmail.com>
2022-12-09 03:28:29 -0600

    added basic portfolio

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default[
+  {
+    title: 'Portfolio Website',
+    imgUrl: '/assets/dev-portfolio.jpg',
+    stack: ['React', 'Tailwind', 'Vite'],
+    link: '',
+  },
+  {
+    title: 'Handy Hand Me Downs',
+    imgUrl: '',
+    stack: ['Ruby on Rails', 'Tailwind'],
+    link: '',
+  },
+  {
+    title: 'Other Project',
+    imgUrl: '',
+    stack: ['TBD'],
+    link: '',
+  },
+]
