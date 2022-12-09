28f4e7b612614e88629a84d3d6bbc7ca03172a05
Simeon Marchand <simeonrmarchand@gmail.com>
2022-12-09 03:50:42 -0600

    timeline changes

---
+++ src/data/timeline.js
@@ -0,0 +1,14 @@
+export default[
+  {
+    year:'June 2023',
+    title: "Associate's of Software Development",
+    duration: '0 years',
+    details: 'As a prerequisite to seeking my BS of Computer Science. I had the opportunity to learn basic C++ throughout my time completing my Associates. I also was able to use my knowledge learned in my HTML, CSS, and JS class to create this website. I learned database fundamentals, data structure basics, and networking.',
+  },
+  {
+    year:'December 2024',
+    title: "Bachelor's of Computer Science",
+    duration: '0 years',
+    details: 'I pursued the knowledge that a computer science degree had to offer. I did this as I have always been interested in computers, programing, and creating. I am excited for what I will learn along the way of during my education.',
+  }
+]
